90b3b786f336c6b58e2bdea5479543a41b750107
Andreas Eger <dev@eger-andreas.de>
2014-04-16 17:47:52 +0200

    wip config and config_loader
    these are selective copies from rubocop although quite a lot simpler

---
+++ lib/codeqa/config.rb
@@ -0,0 +1,33 @@
+module Codeqa
+  class Config# < DelegateClass(Hash)
+    include Singleton
+
+    def self.load
+      self.instance.set_config(ConfigLoader.get_config_hash)
+    end
+
+    def set_config(hash)
+      @hash = hash
+    end
+    def excluded?(file)
+      file = File.join(Dir.pwd, file) unless file.start_with?('/')
+      patterns_to_exclude.any? { |pattern| match_path?(pattern, file) }
+    end
+    def enabled?(checker)
+      !!@hash[checker.to_s]['Enabled']
+    end
+  private
+    def patterns_to_exclude
+      @hash['Exclude']
+    end
+    def match_path?(pattern, path)
+      case pattern
+      when String
+        basename = File.basename(path)
+        path == pattern || basename == pattern || File.fnmatch(pattern, path)
+      when Regexp
+        path =~ pattern
+      end
+    end
+  end
+end
