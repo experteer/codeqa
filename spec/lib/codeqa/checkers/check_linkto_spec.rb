a9cf2c109b3dbcc569c5ecb51449b2c7ec8ec065
Andreas Eger <dev@eger-andreas.de>
2014-05-16 17:25:56 +0200

    fix chmod permissions bits
    fix error in check_linkto_spec

--- spec/lib/codeqa/checkers/check_linkto_spec.rb
+++ spec/lib/codeqa/checkers/check_linkto_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe Codeqa::Checkers::CheckLinkto do
-  it "should check text files" do
-    source = source_with
+  it "should check erb files" do
+    source = source_with('','file.html.erb')
     described_class.check?(source).should be_true
     source = source_with('', 'zipped.zip')
     described_class.check?(source).should be_false
   end
 
   it "should detect <% link_to ... do ... %>" do
-    source = source_with("<% link_to '/page',do_some_paths do%>")
+    source = source_with("<% link_to '/page',do_some_paths do%>",'file.html.erb')
     checker = check_with(described_class, source)
     checker.should be_error
     checker.errors.details.should be == [["1,1", "old style block link_to in line 1"]]
   end
 
   it "should find not find if not there " do
-    source = source_with("<%= link_to '/page',do_some_paths do%>")
+    source = source_with("<%= link_to '/page',do_some_paths do%>",'file.html.erb')
     checker = check_with(described_class, source)
     checker.should be_success
   end
