1c94ab00d2ac367ca8d44673daef4ef19aa9471f
Andreas Eger <dev@eger-andreas.de>
2014-08-18 17:06:03 +0200

    minor changes to projects codeqa settings

--- .codeqa.rb
+++ .codeqa.rb
@@ -4,7 +4,9 @@ Codeqa.configure do |config|
                      'spec/fixtures/ruby_error.rb',
                      'lib/codeqa/checkers/check_pry.rb',
                      'spec/lib/codeqa/checkers/check_pry_spec.rb',
-                     'spec/lib/codeqa/checkers/check_rspec_focus_spec.rb']
+                     'spec/lib/codeqa/checkers/check_rspec_focus_spec.rb',
+                     'coverage/*',
+                     'pkg/*']
 
   config.enabled_checker.delete 'CheckRubySyntax'
   config.enabled_checker << 'RubocopLint'
