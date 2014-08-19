df901c223a4faf97a2f96b7168e0867cf69dbf5d
Andreas Eger <dev@eger-andreas.de>
2014-08-19 09:27:56 +0200

    rbx can't deal with %i()

--- spec/support/checker.rb
+++ spec/support/checker.rb
@@ -1,19 +1,19 @@
 RSpec.shared_examples 'a checker' do
   let(:checker){ described_class.new(double('SourceFile', :null_object => true)) }
-  %i(name hint check).each do |method|
+  %w(name hint check).each do |method|
     it "should respond to :#{method}" do
-      expect(checker).to respond_to(method)
+      expect(checker).to respond_to(method.to_sym)
     end
   end
-  %i(name hint).each do |method|
+  %w(name hint).each do |method|
     it "should return a non empty string from :#{method}" do
-      expect(checker.send(method)).not_to be_empty
+      expect(checker.send(method.to_sym)).not_to be_empty
     end
   end
   context 'ClassMethods' do
-    %i(check? available?).each do |method|
+    %w(check? available?).each do |method|
       it "should respond to :#{method}" do
-        expect(described_class).to respond_to(method)
+        expect(described_class).to respond_to(method.to_sym)
       end
     end
   end
